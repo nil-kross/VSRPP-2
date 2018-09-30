4cef0f825808ee986bf93e1623e92a70093817ad
nil-kross <39334605+nil-kross@users.noreply.github.com>
2018-09-30 12:44:48 +0300

    Add 'NYT Requester' project

---
+++ NYT Requester/ApiKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NYT Requester
+{
+    public static class ApiKey
+    {
+        public override String ToString()
+        {
+            return "35858170926d48328dcb474ba924b00e";
+        }
+    }
+}
---
+++ NYT Requester/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// http://developer.nytimes.com/books_api.json#/Console/GET/lists/best-sellers/history.json
+
+namespace Lomtseu.Requester.NYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
